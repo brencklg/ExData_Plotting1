b1cfdde709d53cf34759e58b9bed588454f31347
brencklg <george.brenckle@gmail.com>
2016-02-04 21:05:13 -0500

    Created first graph
    plot1.R and plot1.png

---
+++ plot1.R
@@ -0,0 +1,8 @@
+#Set the working directory correctly
+setwd("~/Desktop/RProjects/Coursera/ExploratoryDataAnalysis/Week1/ExData_Plotting1")
+list.files()
+
+# first load in the data from p2.rda
+p2 <- read.csv(file='p2.csv')
+
+
